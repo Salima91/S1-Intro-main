91f307389673b564e33ea90822651765e52526ad
salima <1998187@cegepoutaouais.qc.ca>
2023-01-16 11:52:04 -0500

    deuxieme commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Bonjour");
+
+function ajouter(){
+    // ajout de la valeur estimée
+    let item= document.getElementById("desc").value;
+    // ajout de la valeur estimée
+    let valeur= document.getElementById("valeur").value;
+   let  nouveaup=document.createElement("p");
+    nouveauValeur=document.createElement("span");
+    nouveaup.innerText=item +" : ";
+    nouveauValeur.innerText= valeur + "$";
+    document.getElementById("maliste").append(nouveaup);
+    nouveaup.append(nouveauValeur);
+
+// modificATION DU TOTAL
+    let ancienPrix=document.getElementById("total").innerText;
+    let nouveauPrix= +ancienPrix + +valeur;// variable : convertir en numerique
+    document.getElementById("total").innerText=nouveauPrix;
+
+
+}
+// creer une balise p// ecrire dans le para et ajouter le dans la liste
+function annuler() {
+
+
+}
